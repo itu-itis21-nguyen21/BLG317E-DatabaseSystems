c936670e9a875f8b9a59d3581514eb0891bc1a9f
weronikajopek <weronikajopek19@gmail.com>
2024-12-29 20:14:13 +0100

    29.12.24 : last css changes

--- new_app/database.py
+++ new_app/database.py
@@ -1,3 +1,3 @@
 import mysql.connector as connector
 
-connection = connector.connect(host="localhost", user="root", password="123456", database="un_stats23")
+connection = connector.connect(host="localhost", user="root", password="11010607", database="un_stats23")
